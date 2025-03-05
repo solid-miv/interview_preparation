b654b881deedb57b22e40ce4f3cef27952d524fa
solid_miv <solid.miv@gmail.com>
2025-03-05 17:37:15 +0100

    add 9.4.py

---
+++ ace_data_science_interview/coding/easy/9.4.py
@@ -0,0 +1,22 @@
+from heapq import heappush, heappop
+from typing import List
+
+# O(k^2*log(k)) time complexity (if k << len(arr))
+def get_kth_smallest(arr: List[List[int]], k: int):
+    heap = []
+    res = -1
+
+    for i in range(min(k, len(arr))):
+        for j in range(min(k, len(arr))):
+            heappush(heap, arr[i][j])
+
+    for _ in range(k):
+        res = heappop(heap)
+    
+    return res
+
+if __name__ == "__main__":
+    A = [[1, 4, 7],
+         [3, 5, 9],
+         [6, 8, 11]]
+    print(get_kth_smallest(A, 6))
